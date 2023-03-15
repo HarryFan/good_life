25b8801308dcb7bdbb87ee15558531ef081d6940
Harry <harry750110@gmail.com>
2023-03-15 18:54:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  
+    <GoodLife />
+  </div>
+</template>
+
+<script>
+import GoodLife from './components/GoodLife.vue';
+
+export default {
+  name: 'App',
+  components: {
+
+    GoodLife
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GoodLife.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- 使用 Vue 的模板語言 -->
+  <div class="container">
+    <h3>好文</h3>
+    <ul>
+      <!-- 使用 v-for 指令渲染列表 -->
+      <li v-for="(item, index) in items" :key="index">
+        <h4>{{ item.title }}</h4>
+        <p>{{ item.quote }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  // 導入 contents.json 文件
+  import contents from '../json/contents.json'
+
+  export default {
+    // 設置名稱為 GoodLife 的 Vue 組件
+    name: 'GoodLife',
+    data() {
+      // 定義 items 數據
+      return {
+        items: contents
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  /* 使用 Sass */
+  .container {
+    position: relative;
+    margin: 0 auto;
+    max-width: 960px;
+    text-align: center;
+
+    li {
+      margin: 30px auto;
+      position: relative;
+      list-style-type: none;
+    }
+  }
+</style>
